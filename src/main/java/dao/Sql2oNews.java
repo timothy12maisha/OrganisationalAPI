9edf8e03713443856bae69ebe8d0600a9627009a
timothy12maisha <timomaisha@gmail.com>
2020-02-20 08:32:43 +0300

    added changes to the user interface to link SQL

--- src/main/java/dao/Sql2oNews.java
+++ src/main/java/dao/Sql2oNews.java
@@ -19,7 +19,7 @@ public class Sql2oNews implements NewsInterface {
             int id = (int) con.createQuery(sql, true)
                     .addParameter("title", news.getTitle())
                     .addParameter("description", news.getDescription())
-                    .addParameter("author", news.getAuthor())
+                    .addParameter("auhor", news.getAuthor())
                     .executeUpdate()
                     .getKey();
 
